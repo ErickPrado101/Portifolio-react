e44825e3603bad53d51c75390eaedf242ec48029
Erick_Prado404 <73625446+ErickPrado101@users.noreply.github.com>
2024-11-20 20:20:08 -0300

    Atualizar o Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -32,7 +32,7 @@ const Projetos = () => {
   })
 
   return (
-    <section id="projetos" ref={ref} className="py-20 bg-gray-900">
+    <section id="project" ref={ref} className="py-20 bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 50 }}
